45b2f6192f33da73776d6bf0413a9d11538efb47
VHari163 <vinayakav820@gmail.com>
2026-06-11 10:09:53 +0530

    Initial commit

---
+++ HTML_Programes/Day -4/script.js
@@ -0,0 +1,14 @@
+let name = "Hari";      
+var age = 26;
+const height = "1.72" ;   /* no option to change it */
+console.log(name);        /* any thing can be printed using console.log(); */
+console.log(height);
+console.log(age);
+console.log(age+2);
+console.log(age-2);
+console.log(age*2);
+console.log(age%2);
+console.log(age/2);
+age += 3;
+age = age+3;
+console,log(age);
